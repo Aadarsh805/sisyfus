175211f6c1811de94d41b0cbc6ddcb912c5ce7fd
leostelon <leostelon17@gmail.com>
2024-11-11 14:15:05 +0530

    compute stealth key

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,7 +1,11 @@
 // Utility functions from
 // https://github.com/ScopeLift/stealth-address-sdk/tree/main/src/utils/crypto
 import * as secp256k1 from "@noble/secp256k1";
-import { hexToBytes } from "viem";
+import { hexToBytes, keccak256 } from "viem";
+import {
+  publicKeyToAddress as publicKeyToAddressViem,
+  bytesToHex,
+} from "viem/utils";
 
 function randomPrivateKey() {
   var randPrivateKey = secp.utils.randomPrivateKey();
@@ -52,6 +56,53 @@ export function getStealthPublicKey({ spendingPublicKey, hashedSharedSecret }) {
   return secp.ProjectivePoint.fromHex(spendingPublicKey.replace("0x", ""))
     .add(hashedSharedSecretPoint)
     .toRawBytes(false);
+}
+
+export function computeStealthKey({
+  ephemeralPublicKey,
+  spendingPrivateKey,
+  viewingPrivateKey,
+}) {
+  const sharedSecret = secp.getSharedSecret(
+    viewingPrivateKey.replace("0x", ""),
+    ephemeralPublicKey.replace("0x", "")
+  );
+
+  const hashedSharedSecret = getHashedSharedSecret({ sharedSecret });
+  console.log("Compute Stealth Hashed Secret:", hashedSharedSecret);
+
+  const spendingPrivateKeyBigInt = BigInt(spendingPrivateKey);
+  const hashedSecretBigInt = BigInt(hashedSharedSecret);
+
+  // Compute the stealth private key by summing the spending private key and the hashed shared secret.
+  const stealthPrivateKeyBigInt = addPriv({
+    a: spendingPrivateKeyBigInt,
+    b: hashedSecretBigInt,
+  });
+
+  const stealthPrivateKeyHex = `0x${stealthPrivateKeyBigInt
+    .toString(16)
+    .padStart(64, "0")}`;
+
+  return stealthPrivateKeyHex;
+}
+
+export function publicKeyToAddress({ publicKey }) {
+    // Use viem to convert the public key to an address
+    return publicKeyToAddressViem(
+      typeof publicKey !== "string"
+        ? bytesToHex(publicKey)
+        : publicKey.replace("0x", "")
+    );
+  }
+
+export function getHashedSharedSecret({ sharedSecret }) {
+  return keccak256(sharedSecret);
+}
+
+function addPriv({ a, b }) {
+  const curveOrderBigInt = BigInt(secp256k1.CURVE.n);
+  return (a + b) % curveOrderBigInt;
 }
 
 function uintArrayToHex(uintArray) {
