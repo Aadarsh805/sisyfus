04561e0d69a245870b9e22afb60bbc234a57e429
leostelon <leostelon17@gmail.com>
2024-11-11 13:11:38 +0530

    generate random stealth meta address

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -15,3 +15,28 @@ export function getEphemeralKey() {
   );
   return ["0x" + ephemeralPrivateKey.toString(16), "0x" + ephemeralPublicKey];
 }
+
+export function generateRandomStealthMetaAddress() {
+  const spendingPrivateKey = randomPrivateKey();
+  const viewingPrivateKey = randomPrivateKey();
+  const spendingPublicKey = uintArrayToHex(
+    secp.getPublicKey(spendingPrivateKey, (isCompressed = true))
+  );
+  const viewingPublicKey = uintArrayToHex(
+    secp.getPublicKey(viewingPrivateKey, (isCompressed = true))
+  );
+  const stealthMetaAddress = "st:eth:0x" + spendingPublicKey + viewingPublicKey;
+  return [
+    "0x" + spendingPrivateKey.toString(16),
+    "0x" + viewingPrivateKey.toString(16),
+    "0x" + spendingPublicKey,
+    "0x" + viewingPublicKey,
+    stealthMetaAddress,
+  ];
+}
+
+function uintArrayToHex(uintArray) {
+  return Array.from(uintArray) // Convert the Uint8Array to a regular array
+    .map((byte) => byte.toString(16).padStart(2, "0")) // Convert each byte to a 2-digit hex string
+    .join("");
+}
